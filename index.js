3ccf17f5ba34d67f785af9afc669316b5c2cb2ae
Premium Partners <145288968+redaqsd@users.noreply.github.com>
2024-06-22 16:50:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 const express = require("express")
 const app = express()
 require("dotenv").config()
+app.use(express.json())
+const mongoose = require("mongoose")
+const Dikr = require("./schema")
 const cors = require("cors")
-const connectDB = require("./db/connectDB")
-const route = require("./router/route")
 
 app.use(cors())
 app.use(express.json())
-app.use("/api/v1/dikr" , route)
 
-app.get("/" , (req , res) => {
-    res.status(200).send("hello")
+
+app.get("/api/v1/dikr", async(req, res) => {
+    const dikr = await Dikr.find({})
+    res.status(200).json({dikr})
 })
 
-app.get("*" , (req , res) => {
-    res.status(404).send("Route Does Not Exist")
-})
+
+const connectDB = async (url) => {
+    try {
+        await mongoose.connect(url)
+        console.log("connected")
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error)
+    }
+}
 
 const PORT = process.env.PORT || 3000
 
